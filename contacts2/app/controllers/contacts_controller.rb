2c4a6dcc99258343a86b527cc5519f097f59bf15
Marcus Mitchell <mdeuce@hotmail.com>
2019-01-26 16:52:58 +0000

    rails contact app

---
+++ contacts2/app/controllers/contacts_controller.rb
@@ -0,0 +1,54 @@
+class ContactsController < ApplicationController
+    def home
+    end
+    
+    def index
+        @contacts = Contact.all
+    end
+    
+    def new
+    end
+    
+    def create
+        contact = Contact.new
+        contact.first_name = params[:first_name]
+        contact.last_name = params[:last_name]
+        contact.phone = params[:phone]
+        contact.email = params[:email]
+        contact.address = params[:address]
+        contact.city = params[:city]
+        contact.state = params[:state]
+        contact.zip = params[:zip]
+        contact.save
+        redirect_to '/contacts'
+    end
+    
+    def edit
+        @edit = Contact.find_by(id: params[:id])
+    end
+    
+    def update
+        contact = Contact.find_by(id: params[:id])
+        contact.first_name = params[:first_name]
+        contact.last_name = params[:last_name]
+        contact.phone = params[:phone]
+        contact.email = params[:email]
+        contact.address = params[:address]
+        contact.city = params[:city]
+        contact.state = params[:state]
+        contact.zip = params[:zip]
+        contact.save
+        redirect_to '/contacts'
+    end
+    
+    def delete
+        contact = Contact.find_by(id: params[:id])
+        contact.save
+        redirect_to '/contacts'
+    end
+    
+    def results
+        query = "%#{params[:result]}%"
+        @result = Contact.where('first_name LIKE ? OR last_name LIKE ?', query, query)
+    end
+end
--- contacts2/config/routes.rb
+++ contacts2/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/' => 'contacts#home'
+  get '/contacts' => 'contacts#index'
+  get '/contacts/new' => 'contacts#new'
+  post '/contacts/new' => 'contacts#create'
+  get '/contacts/edit' => 'contacts#edit'
+  post 'contacts/edit' => 'contacts#update'
+  get 'contacts/delete'=> 'contacts#delete'
+  post '/contacts/results' => 'contacts#results'
 end
