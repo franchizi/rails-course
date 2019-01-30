04db9ad12fa06461c87b7d836a3adcb83fce4ba7
Marcus Mitchell <mdeuce@hotmail.com>
2019-01-30 02:09:19 +0000

    slight update to school app

--- school/config/routes.rb
+++ school/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get '/courses/new' => 'courses#new'
   post '/courses' => 'courses#create'
   get '/courses' => 'courses#index'
-  get '/courses/edit' => 'courses#edit'
+  get '/courses/:id' => 'courses#edit'
   post '/courses/edit' => 'courses#update'
   get '/courses/delete' => 'courses#delete'
   get '/courses/show' => 'courses#show'
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
   get '/students/edit' => 'students#edit'
   post '/students/edit' => 'students#update'
   get '/students/delete' => 'students#delete'
+  root 'courses#index'
 end
