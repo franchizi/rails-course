5652483b41aa88ed37a24bde7d90dcc4e2d59893
Marcus Mitchell <mdeuce@hotmail.com>
2019-02-03 23:02:29 +0000

    rails places second commit

---
+++ places/app/controllers/places_controller.rb
@@ -0,0 +1,42 @@
+class PlacesController < ApplicationController
+    def index
+        @places = Place.all
+    end
+    
+    def show 
+        @place = Place.find(params[:id])
+        @users = User.all
+    end
+    
+    def new 
+        @place = Place.new
+    end
+    
+    def create 
+        place = Place.new
+        place.name = params[:place][:name]
+        place.picture = params[:place][:picture]
+        place.description = params[:place][:description]
+        place.save
+        redirect_to '/places'
+    end
+    
+    def edit 
+        @place = Place.find(params[:id])
+    end
+    
+    def update 
+        place = Place.find(params[:id])
+        place.name = params[:place][:name]
+        place.picture = params[:place][:picture]
+        place.description = params[:place][:description]
+        place.save
+        redirect_to '/places'
+    end
+    
+    def destroy
+        place = Place.find(params[:id])
+        place.destroy
+    end
+    
+end
